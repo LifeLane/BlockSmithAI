07c4bff9e0b07fb5187df4107519b4a19670a950
Rajib Singh <connectedtorajib@gmail.com>
2025-07-08 11:02:26 +0000

    Content Security Policy of your site blocks the use of 'eval' in JavaScr

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,18 @@
+
 import type {NextConfig} from 'next';
+
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
+    font-src 'self' https://fonts.gstatic.com;
+    img-src 'self' data: https://placehold.co;
+    connect-src 'self' https://api.binance.com https://newsapi.org;
+    frame-src 'none';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+`;
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -17,6 +31,19 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
     ],
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: cspHeader.replace(/\s{2,}/g, ' ').trim(),
+          },
+        ],
+      },
+    ];
   },
 };
 
