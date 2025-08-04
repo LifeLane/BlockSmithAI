d87a2accb85d2120d31b2a2e60eb560e48013d8a
Rajib Singh <connectedtorajib@gmail.com>
2025-08-04 11:13:29 +0000

    update api

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -7,8 +7,8 @@ export const groqModel = 'llama3-70b-8192';
 
 export const ai = genkit({
   plugins: [
-    googleAI(),
-    groq(),
+    googleAI({apiKey: process.env.GEMINI_API_KEY}),
+    groq({apiKey: process.env.GROQ_API_KEY}),
   ],
   logLevel: 'debug',
   enableTracingAndMetrics: true,
