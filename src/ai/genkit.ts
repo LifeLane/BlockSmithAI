db3af462eac000554e0ffb587e4f2051b73e8c21
Rajib Singh <connectedtorajib@gmail.com>
2025-08-04 11:11:41 +0000

    fix

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,7 +2,7 @@ import {genkit} from 'genkit';
 import {groq} from 'genkitx-groq';
 import {googleAI} from '@genkit-ai/googleai';
 
-export const geminiModel = 'gemini-1.5-flash-latest';
+export const geminiModel = 'googleai/gemini-1.5-flash-latest';
 export const groqModel = 'llama3-70b-8192';
 
 export const ai = genkit({
