c72e5045cf9f5310ea571c154a4b4825d8fc64a4
Rajib Singh <connectedtorajib@gmail.com>
2025-08-04 10:18:47 +0000

    no signal only error

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,9 +2,12 @@ import {genkit} from 'genkit';
 import {groq} from 'genkitx-groq';
 
 export const ai = genkit({
-  plugins: [groq()],
-  // By defining the models here, we can reference them in our flows.
-  // Genkit will automatically use the next model in the list as a fallback
-  // if the first one is unavailable or overloaded.
-  model: ['groq/llama3-70b-8192', 'groq/gemma-7b-it'],
+  plugins: [
+    groq({
+      // By defining the models here, we can reference them in our flows.
+      // Genkit will automatically use the next model in the list as a fallback
+      // if the first one is unavailable or overloaded.
+      model: ['llama3-70b-8192', 'gemma-7b-it'],
+    }),
+  ],
 });
